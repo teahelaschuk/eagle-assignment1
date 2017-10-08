71365b379d81852b8d3544c8c6a4e2ab59c05a0d
lancelei <lancelei.h@gmail.com>
2017-10-08 14:13:30 -0700

    info to Info

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,13 +21,13 @@ class Welcome extends Application {
         $this->data['pagetitle'] = 'BirdBrain - Home';
         
         // load models needed
-        $this->load->model('fleetinfo');
-        $this->load->model('flightinfo');
+        $this->load->model('fleetInfo');
+        $this->load->model('flightInfo');
 
         // retrieve info from models
-        $fleet = $this->fleetinfo->all();
-        $flights = $this->flightinfo->all();
-        $airports = $this->flightinfo->allAirports();
+        $fleet = $this->fleetInfo->all();
+        $flights = $this->flightInfo->all();
+        $airports = $this->flightInfo->allAirports();
 
         // save info to data
         $this->data['fleetNum'] = count($fleet);
